22d99b3990945c5e8c8c2466f0e4a4db8200e74f
Toni <tonarie.k@gmail.com>
2025-03-11 21:09:25 -0400

    port 3000

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log('Starting server on port 3003');
+  console.log('Starting server on port 3000');
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // TODO: limit to only the frontend domain
-  await app.listen(3003, '0.0.0.0');
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
